4e52df8d8002d1c138ff79a9a8d9a9163e9275d4
zezei <alesandro.della.janna@gmail.com>
2024-05-01 00:53:31 -0300

    chore: #01 JEST configured

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,28 +2,15 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() =>
+    TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
-  });
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'devsu-angular-test' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('devsu-angular-test');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, devsu-angular-test');
   });
 });
